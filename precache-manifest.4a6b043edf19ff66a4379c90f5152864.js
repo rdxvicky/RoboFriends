d1c55fae14575e78674155ff6bc951555207114b
rdx_vicky <rdxvicky33@gmail.com>
2020-03-28 17:16:48 +0530

    Updates

---
+++ precache-manifest.4a6b043edf19ff66a4379c90f5152864.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71a379a6e7649124ba8183b645f51161",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01612f810ce87b38b3c3",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "d78571eccf9759c8d10d",
+    "url": "/static/css/main.cfdad8d7.chunk.css"
+  },
+  {
+    "revision": "01612f810ce87b38b3c3",
+    "url": "/static/js/2.aef0a387.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.aef0a387.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d78571eccf9759c8d10d",
+    "url": "/static/js/main.3b2ce8e8.chunk.js"
+  },
+  {
+    "revision": "3a172e9daf29f6cc3071",
+    "url": "/static/js/runtime-main.08f98dcf.js"
+  }
+]);
